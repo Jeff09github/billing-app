1576ad6c100bb72479e7b332a6756afaf1c28cc3
Jeff <jeff09.dev@gmail.com>
2022-02-21 23:30:46 +0800

    update and add form

--- lib/presentation/water_bills/customers_waterbills.dart
+++ lib/presentation/water_bills/customers_waterbills.dart
@@ -12,6 +12,8 @@ class CustomersWaterBillsView extends StatefulWidget {
 }
 
 class _CustomersWaterBillsViewState extends State<CustomersWaterBillsView> {
+  final _formKey = GlobalKey<FormState>();
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,7 +34,6 @@ class _CustomersWaterBillsViewState extends State<CustomersWaterBillsView> {
               _buildPopupMenuItem(
                 text: "Create New Bill",
                 value: Choose.createNewBill,
-                // onTap: () {},s
               ),
             ],
           ),
@@ -53,12 +54,57 @@ class _CustomersWaterBillsViewState extends State<CustomersWaterBillsView> {
   }
 
   void _onChoiceSelected(Choose choose) {
-    showModalBottomSheet(
+    showModalBottomSheet<void>(
+      isDismissible: false,
+      isScrollControlled: true,
       context: context,
+      clipBehavior: Clip.hardEdge,
       builder: (BuildContext context) {
         switch (choose) {
           case Choose.addCustomer:
-            return Container();
+            return Padding(
+              padding: MediaQuery.of(context).viewInsets,
+              child: Form(
+                key: _formKey,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 16.0, vertical: 8.0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      TextFormField(
+                        keyboardType: TextInputType.text,
+                        decoration:
+                            const InputDecoration(labelText: 'Full Name'),
+                      ),
+                      TextFormField(
+                        keyboardType: TextInputType.number,
+                        decoration:
+                            const InputDecoration(labelText: 'Current CM'),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          ElevatedButton(
+                            onPressed: () {},
+                            child: const Text('ADD'),
+                          ),
+                          const SizedBox(
+                            width: 12.0,
+                          ),
+                          OutlinedButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: const Text('Cancel'),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            );
           case Choose.createNewBill:
             return Container();
         }
